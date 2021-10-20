70a0a5bb59aa3d3a1a50f3343f98e7f09cdc91ec
Ciaran McCreesh <ciaran.mccreesh@glasgow.ac.uk>
2021-10-20 16:32:48 +0100

    Missing

---
+++ gcs/propagators-fwd.hh
@@ -0,0 +1,15 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#ifndef GLASGOW_CONSTRAINT_SOLVER_GUARD_GCS_PROPAGATORS_FWD_HH
+#define GLASGOW_CONSTRAINT_SOLVER_GUARD_GCS_PROPAGATORS_FWD_HH 1
+
+namespace gcs
+{
+    enum class PropagatorState
+    {
+        Enable,
+        DisableUntilBacktrack
+    };
+}
+
+#endif
