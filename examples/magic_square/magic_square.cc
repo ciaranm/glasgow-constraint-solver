60827332725dffeb02feae4a002e9d663026fb02
Ciaran McCreesh <ciaran.mccreesh@glasgow.ac.uk>
2021-12-05 14:25:03 +0000

    Proper command line

--- examples/magic_square/magic_square.cc
+++ examples/magic_square/magic_square.cc
@@ -11,6 +11,8 @@
 #include <iostream>
 #include <vector>
 
+#include <boost/program_options.hpp>
+
 using namespace gcs;
 
 using std::cerr;
@@ -21,19 +23,58 @@ using std::pair;
 using std::stoi;
 using std::vector;
 
+namespace po = boost::program_options;
+
 auto main(int argc, char * argv[]) -> int
 {
-    int size = 5;
+    po::options_description display_options{ "Program options" };
+    display_options.add_options()
+        ("help", "Display help information")
+        ("prove", "Create a proof")
+        ("all-different", "Use AllDifferent rather than inequalities");
 
-    if (argc > 2) {
-        cerr << "Usage: " << argv[0] << " [ size ]" << endl;
+    po::options_description all_options{ "All options" };
+    all_options.add_options()
+        ("size", po::value<int>()->default_value(5), "Size of the problem to solve")
+        ;
+
+    all_options.add(display_options);
+
+    po::positional_options_description positional_options;
+    positional_options
+        .add("size", -1);
+
+    po::variables_map options_vars;
+
+    try {
+        po::store(po::command_line_parser(argc, argv)
+                .options(all_options)
+                .positional(positional_options)
+                .run(), options_vars);
+        po::notify(options_vars);
+    }
+    catch (const po::error & e) {
+        cerr << "Error: " << e.what() << endl;
+        cerr << "Try " << argv[0] << " --help" << endl;
         return EXIT_FAILURE;
     }
 
-    if (argc >= 2)
-        size = stoi(argv[1]);
+    if (options_vars.count("help")) {
+        cout << "Usage: " << argv[0] << " [options] [size]" << endl;
+        cout << endl;
+        cout << display_options << endl;
+        return EXIT_SUCCESS;
+    }
 
-    Problem p; // { Proof{ "magic_square.opb", "magic_square.veripb" } };
+    cout << "Replicating the MiniCP Magic Square benchmark." << endl;
+    cout << "See Laurent D. Michel, Pierre Schaus, Pascal Van Hentenryck:" << endl;
+    cout << "\"MiniCP: a lightweight solver for constraint programming.\"" << endl;
+    cout << "Math. Program. Comput. 13(1): 133-184 (2021)." << endl;
+    cout << "This should take 6042079 recursions with default options." << endl;
+    cout << endl;
+
+    int size = options_vars["size"].as<int>();
+    Problem p = options_vars.count("prove") ? Problem{ Proof{ "magic_square.opb", "magic_square.veripb" } } : Problem{ };
     Integer m{ size * (size * size + 1) / 2 };
 
     vector<vector<IntegerVariableID> > grid;
@@ -47,11 +88,16 @@ auto main(int argc, char * argv[]) -> int
         }
     }
 
-    for (unsigned x = 0 ; x < grid_flat.size() ; ++x)
-        for (unsigned y = x + 1 ; y < grid_flat.size() ; ++y)
-            p.post(NotEquals{ grid_flat[x], grid_flat[y] });
-
-//    p.post(AllDifferent{ grid_flat });
+    // As far as I can tell, the statistics reported in the paper only make
+    // sense for non-GAC all-different.
+    if (options_vars.count("all-different")) {
+        p.post(AllDifferent{ grid_flat });
+    }
+    else {
+        for (unsigned x = 0 ; x < grid_flat.size() ; ++x)
+            for (unsigned y = x + 1 ; y < grid_flat.size() ; ++y)
+                p.post(NotEquals{ grid_flat[x], grid_flat[y] });
+    }
 
     for (int x = 0 ; x < size ; ++x) {
         Linear coeff_vars;
