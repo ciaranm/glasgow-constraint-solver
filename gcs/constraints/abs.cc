58e8411ae593e9a45e43bb3d59f82f15af206532
Ciaran McCreesh <ciaran.mccreesh@glasgow.ac.uk>
2022-02-15 23:59:56 +0000

    Dedicated propagator for abs

--- gcs/constraints/abs.cc
+++ gcs/constraints/abs.cc
@@ -10,6 +10,7 @@ using namespace gcs;
 
 using std::max;
 using std::min;
+using std::pair;
 
 Abs::Abs(const IntegerVariableID v1, const IntegerVariableID v2) :
     _v1(v1),
@@ -32,14 +33,36 @@ auto Abs::install(Propagators & propagators, const State & initial_state) && -> 
     auto v2u = max(initial_state.upper_bound(_v1), -initial_state.lower_bound(_v1));
     propagators.trim_upper_bound(initial_state, _v2, v2u);
 
-    // _v2 == x <-> _v1 == x || _v1 == -x
-    for (auto x = min(max(0_i, initial_state.lower_bound(_v1)), max(0_i, initial_state.lower_bound(_v2))); x <= v2u; ++x) {
-        if (initial_state.in_domain(_v2, x))
-            propagators.cnf(initial_state, {_v2 != x, initial_state.in_domain(_v1, x) ? Literal{_v1 == x} : FalseLiteral{}, initial_state.in_domain(_v1, -x) ? Literal{_v1 == -x} : FalseLiteral{}}, true);
-        if (initial_state.in_domain(_v1, x))
-            propagators.cnf(initial_state, {_v1 != x, initial_state.in_domain(_v2, x) ? Literal{_v2 == x} : FalseLiteral{}}, true);
-        if (initial_state.in_domain(_v1, -x))
-            propagators.cnf(initial_state, {_v1 != -x, initial_state.in_domain(_v2, x) ? Literal{_v2 == x} : FalseLiteral{}}, true);
+    // _v2 = abs(_v1)
+    Triggers triggers{.on_change = {_v1, _v2}};
+    propagators.propagator(initial_state, [v1 = _v1, v2 = _v2](State & state) -> pair<Inference, PropagatorState> {
+        // v2 = abs(v1)
+        Inference result = Inference::NoChange;
+        state.for_each_value_while(v1, [&](Integer val) {
+            if (! state.in_domain(v2, abs(val)))
+                increase_inference_to(result, state.infer_not_equal(v1, val, JustifyUsingRUP{}));
+            return result != Inference::Contradiction;
+        });
+
+        state.for_each_value_while(v2, [&](Integer val) {
+            if (! state.in_domain(v1, val) && ! state.in_domain(v1, -val))
+                increase_inference_to(result, state.infer_not_equal(v2, val, JustifyUsingRUP{}));
+            return result != Inference::Contradiction;
+        });
+
+        return pair{result, PropagatorState::Enable};
+    }, triggers, "abs");
+
+    if (propagators.want_nonpropagating()) {
+        // _v2 == x <-> _v1 == x || _v1 == -x
+        for (auto x = min(max(0_i, initial_state.lower_bound(_v1)), max(0_i, initial_state.lower_bound(_v2))); x <= v2u; ++x) {
+            if (initial_state.in_domain(_v2, x))
+                propagators.cnf(initial_state, {_v2 != x, initial_state.in_domain(_v1, x) ? Literal{_v1 == x} : FalseLiteral{}, initial_state.in_domain(_v1, -x) ? Literal{_v1 == -x} : FalseLiteral{}}, false);
+            if (initial_state.in_domain(_v1, x))
+                propagators.cnf(initial_state, {_v1 != x, initial_state.in_domain(_v2, x) ? Literal{_v2 == x} : FalseLiteral{}}, false);
+            if (initial_state.in_domain(_v1, -x))
+                propagators.cnf(initial_state, {_v1 != -x, initial_state.in_domain(_v2, x) ? Literal{_v2 == x} : FalseLiteral{}}, false);
+        }
     }
 }
 
