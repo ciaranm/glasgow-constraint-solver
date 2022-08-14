7027f459c1d27667be84ffc18ab572ffefff8c2b
Ciaran McCreesh <ciaran.mccreesh@glasgow.ac.uk>
2022-08-14 23:48:44 +0100

    This should probably be quoted

--- xcsp/run_xcsp_test.bash
+++ xcsp/run_xcsp_test.bash
@@ -9,6 +9,6 @@ progname=$(basename $prog )
 export PATH=$HOME/.local/bin:$PATH
 
 $prog --prove --all $testsdir/$testname.xml > >(tee $testname.out) || exit 1
-grep -q $checkfor $testname.out || exit 1
+grep -q "$checkfor" $testname.out || exit 1
 veripb xcsp.{opb,veripb} || exit 1
 rm -f xcsp.{opb,veripb} $testname.out
