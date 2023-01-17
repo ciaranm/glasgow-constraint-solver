914c8ab4f866a0154e1b81e2565f990e88523f46
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2023-01-17 16:29:35 +0000

    Give ProofOptions ctors so it works with clang and make_optional

--- gcs/proof_options.hh
+++ gcs/proof_options.hh
@@ -15,6 +15,10 @@ namespace gcs
      */
     struct ProofOptions
     {
+        explicit ProofOptions(const std::string &, const std::string &);
+        explicit ProofOptions(const std::string &, const std::string &, bool);
+        ProofOptions(const ProofOptions &) = default;
+
         std::string opb_file;           ///< Filename for the OPB model
         std::string proof_file;         ///< Filename for the proof file
         bool use_friendly_names = true; ///< Use verbose names, rather than just x1, x2, etc.
