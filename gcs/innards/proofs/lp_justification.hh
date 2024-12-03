8ac62d9c37036581177b0de8e49964c075ad7525
mmcilree <matthew.j.mcilree@gmail.com>
2024-12-03 12:13:29 +0000

    First attempt at compute_lp_justification (untested)

--- gcs/innards/proofs/lp_justification.hh
+++ gcs/innards/proofs/lp_justification.hh
@@ -12,9 +12,9 @@ namespace gcs::innards
         const State & state,
         ProofLogger & logger,
         const WeightedPseudoBooleanLessEqual & inference,
-        const std::vector<IntegerVariableID> & dom_vars,
-        const std::vector<IntegerVariableID> & bound_vars,
-        std::map<ProofLine, WeightedPseudoBooleanLessEqual> pb_constraints,
-        const bool use_reason = false) -> ExplicitJustificationFunction;
+        const std::vector<SimpleIntegerVariableID> & dom_vars,
+        const std::vector<SimpleIntegerVariableID> & bound_vars,
+        const std::map<ProofLine, WeightedPseudoBooleanLessEqual> & pb_constraints,
+        bool compute_reason = false) -> std::pair<ExplicitJustificationFunction, Reason>;
 }
 #endif // GLASGOW_CONSTRAINT_SOLVER_LP_JUSTIFICATION_HH
