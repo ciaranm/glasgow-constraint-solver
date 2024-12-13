c5e3e0546325efae710533e3aa4d75b454a9449f
Ciaran McCreesh <ciaran.mccreesh@glasgow.ac.uk>
2024-12-13 11:11:41 +0000

    Simpler Abs encoding

--- gcs/constraints/abs.cc
+++ gcs/constraints/abs.cc
@@ -44,28 +44,27 @@ auto Abs::install(Propagators & propagators, State & initial_state,
     auto v2u = max(initial_state.upper_bound(_v1), -initial_state.lower_bound(_v1));
     propagators.trim_upper_bound(initial_state, optional_model, _v2, v2u, "Abs");
 
-    optional<ProofFlag> selector;
-    if (optional_model)
-        selector = optional_model->create_proof_flag("abs");
-
     // _v2 = abs(_v1)
     Triggers triggers{.on_change = {_v1, _v2}};
-    propagators.install([v1 = _v1, v2 = _v2, selector = selector](
+    propagators.install([v1 = _v1, v2 = _v2](
                             const State & state, auto & inference, ProofLogger * const logger) -> PropagatorState {
         // we're not dealing with bounds. remove from v1 any value whose absolute value
         // isn't in v2's domain.
         for (const auto & val : state.each_value_mutable(v1))
-            if (! state.in_domain(v2, abs(val)))
+            if (! state.in_domain(v2, abs(val))) {
                 inference.infer_not_equal(logger, v1, val, JustifyUsingRUP{}, Reason{[=]() { return Literals{v2 != abs(val)}; }});
+            }
 
         // now remove from v2 any value whose +/-value isn't in v1's domain.
         for (const auto & val : state.each_value_mutable(v2)) {
             if (! state.in_domain(v1, val) && ! state.in_domain(v1, -val) && state.in_domain(v2, val)) {
                 auto just = [&](const Reason & reason) {
+                    // (v2 == val /\ v1 >= 0) -> v1 == val
                     logger->emit_rup_proof_line_under_reason(reason,
-                        WeightedPseudoBooleanSum{} + 1_i * (*selector) + 1_i * (v2 != val) >= 1_i, ProofLevel::Temporary);
+                        WeightedPseudoBooleanSum{} + 1_i * (v1 < 0_i) + 1_i * (v1 == val) + 1_i * (v2 != val) >= 1_i, ProofLevel::Temporary);
+                    // (v2 == val /\ v1 < 0) -> v1 == -val
                     logger->emit_rup_proof_line_under_reason(reason,
-                        WeightedPseudoBooleanSum{} + 1_i * (! *selector) + 1_i * (v2 != val) >= 1_i, ProofLevel::Temporary);
+                        WeightedPseudoBooleanSum{} + 1_i * (v1 >= 0_i) + 1_i * (v1 != -val) + 1_i * (v2 != val) >= 1_i, ProofLevel::Temporary);
                 };
                 inference.infer_not_equal(logger, v2, val, JustifyExplicitly{just}, Reason{[=]() { return Literals{{v1 != val, v1 != -val}}; }});
             }
@@ -76,9 +75,7 @@ auto Abs::install(Propagators & propagators, State & initial_state,
         triggers, "abs");
 
     if (optional_model) {
-        optional_model->add_constraint(WeightedPseudoBooleanSum{} + 1_i * _v2 + -1_i * _v1 == 0_i, HalfReifyOnConjunctionOf{*selector});
-        optional_model->add_constraint(WeightedPseudoBooleanSum{} + 1_i * _v1 + 1_i * _v2 == 0_i, HalfReifyOnConjunctionOf{! *selector});
-        optional_model->add_constraint(WeightedPseudoBooleanSum{} + 1_i * _v1 <= -1_i, HalfReifyOnConjunctionOf{! *selector});
-        optional_model->add_constraint(WeightedPseudoBooleanSum{} + -1_i * _v1 <= 0_i, HalfReifyOnConjunctionOf{*selector});
+        optional_model->add_constraint("Abs", "non-negative", WeightedPseudoBooleanSum{} + 1_i * _v2 + -1_i * _v1 == 0_i, HalfReifyOnConjunctionOf{_v1 >= 0_i});
+        optional_model->add_constraint("Abs", "negative", WeightedPseudoBooleanSum{} + 1_i * _v2 + 1_i * _v1 == 0_i, HalfReifyOnConjunctionOf{_v1 < 0_i});
     }
 }
