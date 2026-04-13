46bb0a13219c651f014a6892d112e1a1198b2543
mmcilree <matthew.j.mcilree@gmail.com>
2026-04-13 18:36:30 +0100

    Fix unique auto table name.

--- gcs/innards/propagators.cc
+++ gcs/innards/propagators.cc
@@ -189,8 +189,9 @@ auto Propagators::define_and_install_table(State & state, ProofModel * const opt
 
         auto selector = state.allocate_integer_variable_with_state(0_i, Integer(depointinate(permitted).size() - 1));
         if (optional_model)
-            optional_model->set_up_integer_variable(selector, 0_i, Integer(depointinate(permitted).size() - 1), "aux_table",
-                    IntegerVariableProofRepresentation::DirectOnly);
+            optional_model->set_up_integer_variable(selector, 0_i, Integer(depointinate(permitted).size() - 1),
+                "aux_table" + to_string(selector.index),
+                IntegerVariableProofRepresentation::DirectOnly);
 
         // pb encoding, if necessary
         if (optional_model) {
