d6143bdbc9c0fa78f0d46d1729694d5e9bce1bc2
Phil Rodgers <philip.rodgers@glasgow.ac.uk>
2026-04-24 14:22:55 +0100

    NValue

--- gcs/constraints/n_value.hh
+++ gcs/constraints/n_value.hh
@@ -4,6 +4,8 @@
 #include <gcs/constraint.hh>
 #include <gcs/variable_id.hh>
 
+#include <list>
+#include <map>
 #include <vector>
 
 namespace gcs
@@ -20,6 +22,10 @@ namespace gcs
     private:
         IntegerVariableID _n_values;
         const std::vector<IntegerVariableID> _vars;
+        std::map<Integer, std::list<IntegerVariableID>> _all_possible_values;
+        virtual auto define_proof_model(innards::ProofModel &) -> void override;
+        virtual auto install_propagators(innards::Propagators &) -> void override;
+        virtual auto prepare(innards::Propagators &, innards::State &, innards::ProofModel * const) -> bool override;
 
     public:
         explicit NValue(const IntegerVariableID &, std::vector<IntegerVariableID>);
