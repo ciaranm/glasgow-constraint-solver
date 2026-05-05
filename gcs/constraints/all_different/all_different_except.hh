9cd153379049669f11c493c382af118daaedb485
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2026-05-06 00:01:37 +0100

    Split AllDifferentExcept::install into prepare / define_proof_model / install_propagators

--- gcs/constraints/all_different/all_different_except.hh
+++ gcs/constraints/all_different/all_different_except.hh
@@ -2,9 +2,13 @@
 #define GLASGOW_CONSTRAINT_SOLVER_GUARD_GCS_CONSTRAINTS_ALL_DIFFERENT_EXCEPT_HH
 
 #include <gcs/constraint.hh>
+#include <gcs/innards/proofs/proof_logger.hh>
+#include <gcs/innards/proofs/proof_only_variables.hh>
 #include <gcs/integer.hh>
 #include <gcs/variable_id.hh>
 
+#include <map>
+#include <memory>
 #include <vector>
 
 namespace gcs
@@ -27,6 +31,17 @@ namespace gcs
     private:
         std::vector<IntegerVariableID> _vars;
         std::vector<Integer> _excluded;
+        std::vector<IntegerVariableID> _sanitised_vars;
+        std::vector<Integer> _sanitised_excluded;
+        std::vector<Integer> _compressed_vals;
+        std::vector<IntegerVariableID> _duplicated_vars;
+        bool _has_duplicates = false;
+        std::shared_ptr<std::map<Integer, innards::ProofLine>> _value_am1_constraint_numbers;
+        std::map<IntegerVariableID, innards::ProofFlag> _duplicate_selectors;
+
+        virtual auto prepare(innards::Propagators &, innards::State &, innards::ProofModel * const) -> bool override;
+        virtual auto define_proof_model(innards::ProofModel &) -> void override;
+        virtual auto install_propagators(innards::Propagators &) -> void override;
 
     public:
         explicit AllDifferentExcept(std::vector<IntegerVariableID> vars, std::vector<Integer> excluded);
