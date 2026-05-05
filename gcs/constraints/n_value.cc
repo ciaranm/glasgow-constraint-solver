81b10cfedc068f235301a4e3d23342be8f7f9d6d
Phil Rodgers <philip.rodgers@glasgow.ac.uk>
2026-05-05 19:53:59 +0100

    Split NValue::install into prepare / define_proof_model / install_propagators

--- gcs/constraints/n_value.cc
+++ gcs/constraints/n_value.cc
@@ -50,6 +50,57 @@ auto NValue::clone() const -> unique_ptr<Constraint>
 
 auto NValue::install(Propagators & propagators, State & initial_state, ProofModel * const optional_model) && -> void
 {
+    if (! prepare(propagators, initial_state, optional_model))
+        return;
+
+    if (optional_model)
+        define_proof_model(*optional_model);
+
+    install_propagators(propagators);
+}
+
+auto NValue::prepare(Propagators &, State & initial_state, ProofModel * const) -> bool
+{
+    for (const auto & var : _vars) {
+        for (auto v : initial_state.each_value_immutable(var))
+            _possible_values.emplace(v, list<IntegerVariableID>{}).first->second.push_back(var);
+    }
+    return true;
+}
+
+auto NValue::define_proof_model(ProofModel & model) -> void
+{
+    list<ProofFlag> flags;
+    for (auto [v, vars] : _possible_values) {
+        auto flag = model.create_proof_flag("nvalue");
+        WPBSum forward;
+        for (auto & var : vars)
+            forward += 1_i * (var == v);
+        forward += 1_i * ! flag;
+        model.add_constraint("NValue", "forward sum", forward >= 1_i);
+
+        WPBSum reverse;
+        for (auto & var : vars)
+            reverse += 1_i * (var != v);
+        reverse += Integer(vars.size()) * flag;
+        model.add_constraint("NValue", "reverse sum", reverse >= Integer(vars.size()));
+
+        flags.push_back(flag);
+    }
+
+    WPBSum forward, reverse;
+    for (auto & flag : flags) {
+        forward += 1_i * flag;
+        reverse += -1_i * flag;
+    }
+    forward += -1_i * _n_values;
+    reverse += 1_i * _n_values;
+    model.add_constraint("NValue", "forward total", forward >= 0_i);
+    model.add_constraint("NValue", "reverse total", reverse >= 0_i);
+}
+
+auto NValue::install_propagators(Propagators & propagators) -> void
+{
     Triggers triggers;
     triggers.on_bounds.emplace_back(_n_values);
     triggers.on_change.insert(triggers.on_change.end(), _vars.begin(), _vars.end());
@@ -80,42 +131,6 @@ auto NValue::install(Propagators & propagators, State & initial_state, ProofMode
         return PropagatorState::Enable;
     },
         triggers, "nvalue");
-
-    if (optional_model) {
-        map<Integer, list<IntegerVariableID>> all_possible_values;
-        for (const auto & var : _vars) {
-            for (auto v : initial_state.each_value_immutable(var))
-                all_possible_values.emplace(v, list<IntegerVariableID>{}).first->second.push_back(var);
-        }
-
-        list<ProofFlag> flags;
-        for (auto [v, vars] : all_possible_values) {
-            auto flag = optional_model->create_proof_flag("nvalue");
-            WPBSum forward;
-            for (auto & var : vars)
-                forward += 1_i * (var == v);
-            forward += 1_i * ! flag;
-            optional_model->add_constraint("NValue", "forward sum", forward >= 1_i);
-
-            WPBSum reverse;
-            for (auto & var : vars)
-                reverse += 1_i * (var != v);
-            reverse += Integer(vars.size()) * flag;
-            optional_model->add_constraint("NValue", "reverse sum", reverse >= Integer(vars.size()));
-
-            flags.push_back(flag);
-        }
-
-        WPBSum forward, reverse;
-        for (auto & flag : flags) {
-            forward += 1_i * flag;
-            reverse += -1_i * flag;
-        }
-        forward += -1_i * _n_values;
-        reverse += 1_i * _n_values;
-        optional_model->add_constraint("NValue", "forward total", forward >= 0_i);
-        optional_model->add_constraint("NValue", "reverse total", reverse >= 0_i);
-    }
 }
 
 auto NValue::s_exprify(const string & name, const innards::ProofModel * const model) const -> string
