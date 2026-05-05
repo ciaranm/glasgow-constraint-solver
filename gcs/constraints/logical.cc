141c3e5be840aa4820fab590ced83d75a06e2f4b
Phil Rodgers <philip.rodgers@glasgow.ac.uk>
2026-05-05 19:37:34 +0100

    Split Logical (And/Or) install into prepare / define_proof_model / install_propagators
    Splits the install_and_or_or helper into install_propagators_logical and
    define_proof_model_logical so And and Or can independently call each
    phase, with _reif_state precomputed in prepare().

--- gcs/constraints/logical.cc
+++ gcs/constraints/logical.cc
@@ -47,168 +47,178 @@ namespace
         return result;
     }
 
-    auto install_and_or_or(Propagators & propagators, const State & initial_state,
-        ProofModel * const optional_model,
-        const Literals & _lits, const Literal & _full_reif,
-        const string & name) -> void
+    auto install_propagators_logical(Propagators & propagators, const Literals & lits,
+        const Literal & full_reif, LiteralIs reif_state, const string & name) -> void
     {
-        auto reif_state = initial_state.test_literal(_full_reif);
         using enum LiteralIs;
 
         if (reif_state == DefinitelyTrue) {
             // definitely true, just force all the literals
-            propagators.install_initialiser([full_reif = _full_reif, lits = _lits](
+            propagators.install_initialiser([full_reif = full_reif, lits = lits](
                                                 const State &, auto & inference, ProofLogger * const logger) {
                 for (auto & l : lits)
                     inference.infer(logger, l, JustifyUsingRUP{}, ReasonFunction{[=]() { return Reason{{full_reif}}; }});
             });
-
-            if (optional_model) {
-                for (auto & l : _lits)
-                    optional_model->add_constraint("Logical", "cnf", Literals{l});
-            }
+            return;
         }
-        else {
-            Triggers triggers;
-            bool saw_false = false;
-            for (auto & l : _lits)
-                overloaded{
-                    [&](const IntegerVariableCondition & cond) {
-                        switch (cond.op) {
-                            using enum VariableConditionOperator;
-                        case Equal:
-                        case NotEqual:
-                            triggers.on_change.push_back(cond.var);
-                            break;
-                        case Less:
-                        case GreaterEqual:
-                            triggers.on_bounds.push_back(cond.var);
-                            break;
-                        }
-                    },
-                    [&](const TrueLiteral &) {
-                    },
-                    [&](const FalseLiteral &) {
-                        saw_false = true;
-                    }}
-                    .visit(l);
 
-            if (saw_false) {
-                // we saw a false literal, the reif variable must be forced off and
-                // then we don't do anything else
-                propagators.install_initialiser([full_reif = _full_reif](
-                                                    const State &, auto & inference, ProofLogger * const logger) -> void {
-                    inference.infer(logger, ! full_reif, JustifyUsingRUP{}, ReasonFunction{});
-                });
-
-                if (optional_model) {
-                    optional_model->add_constraint("Logical", "saw reif false", Literals{! _full_reif});
-                }
-            }
-            else {
-                propagators.install([lits = _lits, full_reif = _full_reif](
-                                        const State & state, auto & inference, ProofLogger * const logger) -> PropagatorState {
-                    switch (state.test_literal(full_reif)) {
-                    case DefinitelyTrue: {
-                        for (auto & l : lits)
-                            inference.infer(logger, l, JustifyUsingRUP{}, ReasonFunction{[=]() { return Reason{{full_reif}}; }});
-                        return PropagatorState::DisableUntilBacktrack;
+        Triggers triggers;
+        bool saw_false = false;
+        for (auto & l : lits)
+            overloaded{
+                [&](const IntegerVariableCondition & cond) {
+                    switch (cond.op) {
+                        using enum VariableConditionOperator;
+                    case Equal:
+                    case NotEqual:
+                        triggers.on_change.push_back(cond.var);
+                        break;
+                    case Less:
+                    case GreaterEqual:
+                        triggers.on_bounds.push_back(cond.var);
+                        break;
                     }
-
-                    case DefinitelyFalse: {
-                        bool any_false = false;
-                        optional<Literal> undecided1;
-
-                        for (auto & l : lits)
-                            switch (state.test_literal(l)) {
-                            case DefinitelyTrue: break;
-                            case DefinitelyFalse: any_false = true; break;
-                            case Undecided:
-                                if (undecided1)
-                                    return PropagatorState::Enable;
-                                else
-                                    undecided1 = l;
-                            }
-
-                        if (any_false)
-                            return PropagatorState::DisableUntilBacktrack;
-                        else if (! undecided1) {
-                            // literals are all true, but reif is false
-                            Reason why;
-                            for (auto & lit : lits)
-                                why.push_back(lit);
-                            why.push_back(! full_reif);
-                            inference.infer(logger, FalseLiteral{}, JustifyUsingRUP{}, ReasonFunction{[=]() { return why; }});
-                            return PropagatorState::Enable;
-                        }
-                        else {
-                            Reason why;
-                            for (auto & l : lits)
-                                if (l != *undecided1)
-                                    why.push_back(l);
-                            why.push_back(! full_reif);
-                            inference.infer(logger, ! *undecided1, JustifyUsingRUP{}, ReasonFunction{[=]() { return why; }});
-                            return PropagatorState::DisableUntilBacktrack;
-                        }
-                    }
-
-                    case Undecided: {
-                        optional<Literal> any_false;
-                        bool all_true = true;
-
-                        for (auto & l : lits)
-                            switch (state.test_literal(l)) {
-                            case DefinitelyTrue: break;
-                            case DefinitelyFalse:
-                                any_false = l;
-                                all_true = false;
-                                break;
-                            case Undecided: all_true = false; break;
-                            }
-
-                        if (any_false) {
-                            inference.infer(logger, ! full_reif, JustifyUsingRUP{}, ReasonFunction{[=]() { return Reason{{! *any_false}}; }});
-                            return PropagatorState::DisableUntilBacktrack;
-                        }
-                        else if (all_true) {
-                            auto justf = [&](const ReasonFunction & reason) {
-                                for (auto & l : lits)
-                                    logger->emit_rup_proof_line_under_reason(reason,
-                                        WPBSum{} + 1_i * l >= 1_i, ProofLevel::Temporary);
-                            };
-                            inference.infer(logger, full_reif, JustifyExplicitlyThenRUP{justf}, ReasonFunction{[=]() {
-                                vector<ProofLiteral> reason_lits{};
-                                for (auto & l : lits)
-                                    reason_lits.emplace_back(l);
-                                return Reason(reason_lits.begin(), reason_lits.end());
-                            }});
-                            return PropagatorState::DisableUntilBacktrack;
-                        }
-                        else
-                            return PropagatorState::Enable;
-                    }
-                    }
-
-                    throw NonExhaustiveSwitch{};
                 },
-                    triggers, name);
+                [&](const TrueLiteral &) {
+                },
+                [&](const FalseLiteral &) {
+                    saw_false = true;
+                }}
+                .visit(l);
 
-                if (optional_model) {
-                    if (DefinitelyFalse != reif_state) {
-                        WPBSum forward;
-                        for (auto & l : _lits)
-                            forward += 1_i * PseudoBooleanTerm{l};
-                        optional_model->add_constraint("Logical", "if condition", forward >= Integer(_lits.size()), HalfReifyOnConjunctionOf{_full_reif});
+        if (saw_false) {
+            // we saw a false literal, the reif variable must be forced off and
+            // then we don't do anything else
+            propagators.install_initialiser([full_reif = full_reif](
+                                                const State &, auto & inference, ProofLogger * const logger) -> void {
+                inference.infer(logger, ! full_reif, JustifyUsingRUP{}, ReasonFunction{});
+            });
+            return;
+        }
+
+        propagators.install([lits = lits, full_reif = full_reif](
+                                const State & state, auto & inference, ProofLogger * const logger) -> PropagatorState {
+            switch (state.test_literal(full_reif)) {
+            case DefinitelyTrue: {
+                for (auto & l : lits)
+                    inference.infer(logger, l, JustifyUsingRUP{}, ReasonFunction{[=]() { return Reason{{full_reif}}; }});
+                return PropagatorState::DisableUntilBacktrack;
+            }
+
+            case DefinitelyFalse: {
+                bool any_false = false;
+                optional<Literal> undecided1;
+
+                for (auto & l : lits)
+                    switch (state.test_literal(l)) {
+                    case DefinitelyTrue: break;
+                    case DefinitelyFalse: any_false = true; break;
+                    case Undecided:
+                        if (undecided1)
+                            return PropagatorState::Enable;
+                        else
+                            undecided1 = l;
                     }
 
-                    Literals reverse;
-                    for (auto & l : _lits)
-                        reverse.push_back(! l);
-                    reverse.push_back(_full_reif);
-                    optional_model->add_constraint("Logical", "if not condition", move(reverse));
+                if (any_false)
+                    return PropagatorState::DisableUntilBacktrack;
+                else if (! undecided1) {
+                    // literals are all true, but reif is false
+                    Reason why;
+                    for (auto & lit : lits)
+                        why.push_back(lit);
+                    why.push_back(! full_reif);
+                    inference.infer(logger, FalseLiteral{}, JustifyUsingRUP{}, ReasonFunction{[=]() { return why; }});
+                    return PropagatorState::Enable;
+                }
+                else {
+                    Reason why;
+                    for (auto & l : lits)
+                        if (l != *undecided1)
+                            why.push_back(l);
+                    why.push_back(! full_reif);
+                    inference.infer(logger, ! *undecided1, JustifyUsingRUP{}, ReasonFunction{[=]() { return why; }});
+                    return PropagatorState::DisableUntilBacktrack;
                 }
             }
+
+            case Undecided: {
+                optional<Literal> any_false;
+                bool all_true = true;
+
+                for (auto & l : lits)
+                    switch (state.test_literal(l)) {
+                    case DefinitelyTrue: break;
+                    case DefinitelyFalse:
+                        any_false = l;
+                        all_true = false;
+                        break;
+                    case Undecided: all_true = false; break;
+                    }
+
+                if (any_false) {
+                    inference.infer(logger, ! full_reif, JustifyUsingRUP{}, ReasonFunction{[=]() { return Reason{{! *any_false}}; }});
+                    return PropagatorState::DisableUntilBacktrack;
+                }
+                else if (all_true) {
+                    auto justf = [&](const ReasonFunction & reason) {
+                        for (auto & l : lits)
+                            logger->emit_rup_proof_line_under_reason(reason,
+                                WPBSum{} + 1_i * l >= 1_i, ProofLevel::Temporary);
+                    };
+                    inference.infer(logger, full_reif, JustifyExplicitlyThenRUP{justf}, ReasonFunction{[=]() {
+                        vector<ProofLiteral> reason_lits{};
+                        for (auto & l : lits)
+                            reason_lits.emplace_back(l);
+                        return Reason(reason_lits.begin(), reason_lits.end());
+                    }});
+                    return PropagatorState::DisableUntilBacktrack;
+                }
+                else
+                    return PropagatorState::Enable;
+            }
+            }
+
+            throw NonExhaustiveSwitch{};
+        },
+            triggers, name);
+    }
+
+    auto define_proof_model_logical(ProofModel & model, const Literals & lits,
+        const Literal & full_reif, LiteralIs reif_state) -> void
+    {
+        using enum LiteralIs;
+
+        if (reif_state == DefinitelyTrue) {
+            for (auto & l : lits)
+                model.add_constraint("Logical", "cnf", Literals{l});
+            return;
         }
+
+        bool saw_false = false;
+        for (auto & l : lits)
+            overloaded{
+                [&](const FalseLiteral &) { saw_false = true; },
+                [&](const auto &) {}}
+                .visit(l);
+
+        if (saw_false) {
+            model.add_constraint("Logical", "saw reif false", Literals{! full_reif});
+            return;
+        }
+
+        if (DefinitelyFalse != reif_state) {
+            WPBSum forward;
+            for (auto & l : lits)
+                forward += 1_i * PseudoBooleanTerm{l};
+            model.add_constraint("Logical", "if condition", forward >= Integer(lits.size()), HalfReifyOnConjunctionOf{full_reif});
+        }
+
+        Literals reverse;
+        for (auto & l : lits)
+            reverse.push_back(! l);
+        reverse.push_back(full_reif);
+        model.add_constraint("Logical", "if not condition", move(reverse));
     }
 }
 
@@ -235,7 +245,29 @@ auto And::clone() const -> unique_ptr<Constraint>
 
 auto And::install(Propagators & propagators, State & initial_state, ProofModel * const optional_model) && -> void
 {
-    install_and_or_or(propagators, initial_state, optional_model, _lits, _full_reif, "and");
+    if (! prepare(propagators, initial_state, optional_model))
+        return;
+
+    if (optional_model)
+        define_proof_model(*optional_model);
+
+    install_propagators(propagators);
+}
+
+auto And::prepare(Propagators &, State & initial_state, ProofModel * const) -> bool
+{
+    _reif_state = initial_state.test_literal(_full_reif);
+    return true;
+}
+
+auto And::define_proof_model(ProofModel & model) -> void
+{
+    define_proof_model_logical(model, _lits, _full_reif, _reif_state);
+}
+
+auto And::install_propagators(Propagators & propagators) -> void
+{
+    install_propagators_logical(propagators, _lits, _full_reif, _reif_state, "and");
 }
 
 auto And::s_exprify(const string & name, const innards::ProofModel * const model) const -> string
@@ -274,11 +306,35 @@ auto Or::clone() const -> unique_ptr<Constraint>
 
 auto Or::install(Propagators & propagators, State & initial_state, ProofModel * const optional_model) && -> void
 {
-    auto lits = _lits;
+    if (! prepare(propagators, initial_state, optional_model))
+        return;
+
+    if (optional_model)
+        define_proof_model(*optional_model);
+
+    install_propagators(propagators);
+}
+
+auto Or::prepare(Propagators &, State & initial_state, ProofModel * const) -> bool
+{
+    _reif_state = initial_state.test_literal(! _full_reif);
+    return true;
+}
+
+auto Or::define_proof_model(ProofModel & model) -> void
+{
+    Literals lits = _lits;
     for (auto & l : lits)
         l = ! l;
+    define_proof_model_logical(model, move(lits), ! _full_reif, _reif_state);
+}
 
-    install_and_or_or(propagators, initial_state, optional_model, lits, ! _full_reif, "or");
+auto Or::install_propagators(Propagators & propagators) -> void
+{
+    Literals lits = _lits;
+    for (auto & l : lits)
+        l = ! l;
+    install_propagators_logical(propagators, move(lits), ! _full_reif, _reif_state, "or");
 }
 
 auto Or::s_exprify(const string & name, const innards::ProofModel * const model) const -> string
--- gcs/constraints/logical.hh
+++ gcs/constraints/logical.hh
@@ -3,6 +3,7 @@
 
 #include <gcs/constraint.hh>
 #include <gcs/innards/literal.hh>
+#include <gcs/innards/state.hh>
 #include <gcs/variable_condition.hh>
 #include <gcs/variable_id.hh>
 
@@ -21,6 +22,11 @@ namespace gcs
     private:
         const innards::Literals _lits;
         const innards::Literal _full_reif;
+        innards::LiteralIs _reif_state = innards::LiteralIs::Undecided;
+
+        virtual auto prepare(innards::Propagators &, innards::State &, innards::ProofModel * const) -> bool override;
+        virtual auto define_proof_model(innards::ProofModel &) -> void override;
+        virtual auto install_propagators(innards::Propagators &) -> void override;
 
     public:
         // Equivalent to And([var != 0 : var in vars], full_reif != 0)
@@ -47,6 +53,11 @@ namespace gcs
     private:
         const innards::Literals _lits;
         const innards::Literal _full_reif;
+        innards::LiteralIs _reif_state = innards::LiteralIs::Undecided;
+
+        virtual auto prepare(innards::Propagators &, innards::State &, innards::ProofModel * const) -> bool override;
+        virtual auto define_proof_model(innards::ProofModel &) -> void override;
+        virtual auto install_propagators(innards::Propagators &) -> void override;
 
     public:
         // Equivalent to Or([var != 0 : var in vars], full_reif != 0)
