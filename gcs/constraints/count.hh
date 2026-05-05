6cc50f3b080e25ec04b45549e3cfde6c58ec5e2f
Phil Rodgers <philip.rodgers@glasgow.ac.uk>
2026-05-05 19:12:49 +0100

    Split Count::install into define_proof_model / install_propagators

--- gcs/constraints/count.hh
+++ gcs/constraints/count.hh
@@ -2,8 +2,10 @@
 #define GLASGOW_CONSTRAINT_SOLVER_GUARD_GCS_CONSTRAINTS_COUNT_HH
 
 #include <gcs/constraint.hh>
+#include <gcs/innards/proofs/proof_only_variables.hh>
 #include <gcs/variable_id.hh>
 
+#include <tuple>
 #include <vector>
 
 namespace gcs
@@ -19,6 +21,10 @@ namespace gcs
     private:
         const std::vector<IntegerVariableID> _vars;
         IntegerVariableID _value_of_interest, _how_many;
+        std::vector<std::tuple<innards::ProofFlag, innards::ProofFlag, innards::ProofFlag>> _flags;
+
+        virtual auto define_proof_model(innards::ProofModel &) -> void override;
+        virtual auto install_propagators(innards::Propagators &) -> void override;
 
     public:
         explicit Count(std::vector<IntegerVariableID>, const IntegerVariableID & value_of_interest, const IntegerVariableID & how_many);
