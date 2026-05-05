5b28551b18f74788305379658b6111f54fcf881b
Phil Rodgers <philip.rodgers@glasgow.ac.uk>
2026-05-05 19:47:33 +0100

    Split Minus::install into define_proof_model / install_propagators

--- gcs/constraints/minus.hh
+++ gcs/constraints/minus.hh
@@ -2,7 +2,11 @@
 #define GLASGOW_CONSTRAINT_SOLVER_GUARD_GCS_CONSTRAINTS_MINUS_HH
 
 #include <gcs/constraint.hh>
+#include <gcs/innards/proofs/proof_logger.hh>
 #include <gcs/variable_id.hh>
+
+#include <optional>
+#include <utility>
 
 namespace gcs
 {
@@ -15,6 +19,10 @@ namespace gcs
     {
     private:
         IntegerVariableID _a, _b, _result;
+        std::pair<std::optional<innards::ProofLine>, std::optional<innards::ProofLine>> _sum_line;
+
+        virtual auto define_proof_model(innards::ProofModel &) -> void override;
+        virtual auto install_propagators(innards::Propagators &) -> void override;
 
     public:
         explicit Minus(IntegerVariableID a, IntegerVariableID b, IntegerVariableID result);
