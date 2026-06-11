04f4a05d905f505c3c6ac03d35f4ea1e0ecad259
Matthew McIlree <matthew.j.mcilree@gmail.com>
2026-06-11 09:48:22 +0100

    Create AssertionAnnotation data structure.

---
+++ gcs/innards/assertion_hints.hh
@@ -0,0 +1,46 @@
+
+#ifndef GLASGOW_CONSTRAINT_SOLVER_GUARD_GCS_ASSERTION_HINTS_HH
+#define GLASGOW_CONSTRAINT_SOLVER_GUARD_GCS_ASSERTION_HINTS_HH
+
+#include <gcs/innards/proofs/proof_line.hh>
+#include <vector>
+namespace gcs::innards
+{
+
+    /**
+     * \brief The allowed names for annotated assertions.
+     *
+     * \ingroup Innards
+     * \sa AssertionAnnotation
+     */
+    enum AssertionHintName
+    {
+        // Hint names types will go here
+    };
+
+    /**
+     * \brief The allowed field types that can appear in the
+     * hints section of an annotated assertion.
+     *
+     * \ingroup Innards
+     * \sa AssertionAnnotation
+     */
+    enum AssertionHintField
+    {
+        // Hint field types will go here
+    };
+
+    /**
+     * \brief An annotation for an annotated assertion step.
+     *
+     * \ingroup Innards
+     */
+    struct AssertionAnnotation
+    {
+        std::vector<ProofLineLabel> derivable_from;
+        AssertionHintName hint_name;
+        std::vector<AssertionHintField> hint_fields;
+    };
+
+} // namespace gcs::innards
+#endif
